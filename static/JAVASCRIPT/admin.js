ebb015252c9c53753ba3f33c0985720e5ef989c5
sharnasatyam6-droid <sharnasatyam6@gmail.com>
2026-09-07 15:15:22 +0530

    Add files via upload

---
+++ static/JAVASCRIPT/admin.js
@@ -0,0 +1,19 @@
+console.log("ParkEase Admin Panel loaded successfully.");
+
+
+// Add Slot button
+document.addEventListener("DOMContentLoaded", function () {
+
+    const addButton = document.querySelector(".add-btn");
+
+    if (addButton) {
+
+        addButton.addEventListener("click", function () {
+
+            alert("Add Parking Slot feature will be connected to the database later.");
+
+        });
+
+    }
+
+});
---
+++ static/JAVASCRIPT/script.js
@@ -0,0 +1,1 @@
+console.log("ParkEase website loaded successfully!");
